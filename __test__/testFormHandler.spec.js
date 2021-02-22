934c97c9c14670531646611ac18d87cb6a6d9846
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-02-22 17:39:26 +0100

    Add files via upload

---
+++ __test__/testFormHandler.spec.js
@@ -0,0 +1,9 @@
+// Import the JavaScript file to test
+import { handleSubmit } from "../src/client/js/formHandler";
+
+describe("Testing the Submit functionality", () => {
+    test("Testing the handleSubmit() function", () => {
+        expect(handleSubmit).toBeDefined();
+    });
+});
+
