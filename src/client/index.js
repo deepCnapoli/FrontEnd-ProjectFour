bc7a1b833c6aca6067233e3043aaca9d0323d375
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-02-24 10:28:34 +0100

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,19 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import { checkForUrl } from './js/checkForUrl'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+//console.log(checkForName);
+
+alert("I EXIST-CT")
+
+export {
+    checkForName,
+    handleSubmit,
+    checkForUrl
+}
