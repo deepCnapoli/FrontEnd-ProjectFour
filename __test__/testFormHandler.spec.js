3f8d249ca724f6ae9e5567422510fea46862d484
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-02-24 15:22:09 +0100

    Delete testFormHandler.spec.js

--- __test__/testFormHandler.spec.js
+++
@@ -1,9 +0,0 @@
-// Import the JavaScript file to test
-import { handleSubmit } from "../src/client/js/formHandler";
-
-describe("Testing the Submit functionality", () => {
-    test("Testing the handleSubmit() function", () => {
-        expect(handleSubmit).toBeDefined();
-    });
-});
-
