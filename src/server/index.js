fc5ae8b73607e918ea6b664ad32d98efc8c9d83e
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-02-24 10:38:16 +0100

    Add files via upload

---
+++ src/server/index.js
@@ -0,0 +1,66 @@
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
+
+const app = express()
+// to use json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(cors());
+
+//app.use(express.json())
+// to use url encoded values
+
+app.use(express.static('dist'));
+
+//Meaningcloud credentials for API
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
+const API_KEY = process.env.API_KEY
+//const API_KEY = "3673be2ada95646265bfc9c9bb08b4fe";
+
+//console.log("mockAPI: "+JSON.stringify(mockAPIResponse))
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+app.get('/test', function (req, res) {
+    res.json(mockAPIResponse);
+})
+
+// POST the URL input to the API
+app.post("/myURL", async (req, res) => {
+  //const bodyURL = req.body.url;
+  const bodyURL = req.body;
+  //const bodyURL ='https://www.theguardian.com/world/2021/feb/21/greek-theatre-director-arrested-on-charges';
+  console.log(bodyURL);
+  const resp = await fetch(`${baseUrl}${API_KEY}&txt=${bodyURL}&lang=en`);
+  //  console.log("resp: "+resp);
+  try {
+    const data = await resp.json();
+   // console.log(data);
+    res.send(data);
+  } catch (err) {
+    console.log("error", err);
+  }
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(3030, function () {
+    console.log('Example app listening on port 3030!')
+})
