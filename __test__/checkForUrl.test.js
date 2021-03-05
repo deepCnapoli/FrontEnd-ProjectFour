ae4713094f1d8fd484514b48f8df878a2bfc52d1
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-03-05 15:47:37 +0100

    Add files via upload

---
+++ __test__/checkForUrl.test.js
@@ -0,0 +1,13 @@
+// file: checkForUrl.test.js
+import { checkForUrl } from "../src/client/js/checkForUrl"
+
+describe ('check Name', () => {
+    test ('should return true', () => {
+        const url='https://google.com'
+        expect (checkForUrl(url)).toBe(true)
+    });
+    test("should return false", () => {
+    	const url = "google . com";
+    	expect(checkForUrl(url)).toBe(false);
+  });
+});
